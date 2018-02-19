edce6faab49d23bc2d35964651caa905e85aee98
mikky-cecil <mikkycecil@gmail.com>
2018-02-18 21:25:10 -0800

    Can pause and adjust game speed now. Machines show up on UI, and turn red when broken.

--- game.js
+++ game.js
@@ -7,21 +7,44 @@ class Laundromat {
 			"dryers" : [],
 			"changeMachines": []
 		};
-		// this.layout = Laundromat.newEmptyLayout(50, 50);
+		this.layout = Laundromat.newEmptyLayout(20, 20);
 	}
 
-	// static newEmptyLayout(width, height){
-	// 	var layout = [];
-	// 	for (var i = 0; i < width; i++){
-	// 		var column = [];
-	// 		for (var j = 0; j < height; j++){
-	// 			column.push(-1);
-	// 		}
-	// 		layout.push(column);
-	// 	}
+	static newEmptyLayout(width, height){
+		var layout = [];
+		for (var i = 0; i < width; i++){
+			var column = [];
+			for (var j = 0; j < height; j++){
+				column.push(-1);
+			}
+			layout.push(column);
+		}
 
-	// 	return layout;
-	// }
+		return layout;
+	}
+
+	addMachineToLayout(machine, x, y){
+		// First check to make sure nothing else is in the way
+		var blocked = false;
+		for (var i = x; i < x + machine.width; i++){
+			for (var j = y; j < y + machine.height; j++){
+				if (this.layout[i][j] != -1){
+					blocked = true;
+					return false;
+				}
+			}
+		}
+
+		// If no block, place the object
+		this.layout[x][y] = machine.objectId;
+		// Add blocks if object is larger than 1x1
+		for (var i = x + 1; i < x + machine.width; i++){
+			for (var j = y + 1; j < y + machine.height; j++){
+				this.layout[i][j] = 1;
+			}
+		}
+		return true;
+	}
 
 	payRent(){
 		// let player go into the negatives for now, will implement debt handling later
@@ -42,6 +65,34 @@ class Laundromat {
 			return (machine.objectId == machineId);
 		});
 	}
+
+	// findMachineCoordinates(machineId){
+	// 	for (var i = 0; i < this.layout.length){
+	// 		for (var j = 0; j < this.layout[0].length){
+	// 			if (this.layout[i][j] == machineId){
+	// 				return new Point(i, j);
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	addMachine(list, newMachine){
+		list.push(newMachine);
+		return newMachine.objectId;
+	}
+
+	addChangeMachine(){
+		return this.addMachine(this.machines.changeMachines, new ChangeMachine(this));
+	}
+
+	addWasher(){
+		return this.addMachine(this.machines.washers, new Washer(this));
+
+	}
+
+	addDryer(){
+		return this.addMachine(this.machines.dryers, new Dryer(this));
+	}	
 }
 
 class Customer {
@@ -97,6 +148,8 @@ class Machine {
 		this.working = true;
 		this.timeUntilFree = 0;
 		this.duration = 0;
+		this.width = 1;
+		this.height = 1;
 	}
 
 	static newId(typeId){
@@ -129,6 +182,8 @@ class Washer extends Machine {
 		this.name = "washer";
 		this.duration = 3;
 		this.cost = 2;
+		this.width = 2;
+		this.height = 2;
 	}
 
 	static newId(){
@@ -153,6 +208,8 @@ class Dryer extends Machine {
 		this.name = "dryer";
 		this.duration = 3;
 		this.cost = 2;
+		this.width = 2;
+		this.height = 2;
 	}
 
 	static newId(){
@@ -219,27 +276,59 @@ class Game {
 		this.day = 1;
 		this.hour = 8;
 		this.minute = 0;
+		this.speed = 100;
 
 		this.laundromat = new Laundromat();
+		this.setupGame();
+		this.startTime();
+
+		this.ui = new UI(this);
+		this.ui.initUI("demoCanvas");
+		this.ui.updateGrid();
+	}
+
+	setupGame(){
 		this.laundromat.money = 6000;
 		this.laundromat.rent = 500;
-		this.laundromat.machines.changeMachines.push(new ChangeMachine(this.laundromat));
-		this.laundromat.machines.changeMachines[0].addChange(this.laundromat, 50);
-		this.laundromat.machines.washers.push(new Washer(this.laundromat));
-		this.laundromat.machines.dryers.push(new Dryer(this.laundromat));
+		
+		var changeMachineId = this.laundromat.addChangeMachine();
+		var changeMachine = this.laundromat.findMachine(changeMachineId);
+		changeMachine.addChange(50);
+		this.laundromat.addMachineToLayout(changeMachine, 0, 0);
+		// this.laundromat.machines.changeMachines[0].addChange(this.laundromat, 50);
+		
+		var washerId = this.laundromat.addWasher();
+		var washer = this.laundromat.findMachine(washerId);
+		this.laundromat.addMachineToLayout(washer, 0, 2);
+		var dryerId = this.laundromat.addDryer();
+		var dryer = this.laundromat.findMachine(dryerId);
+		this.laundromat.addMachineToLayout(dryer, 0, 5);
+		// this.laundromat.machines.washers.push(new Washer(this.laundromat));
+		// this.laundromat.machines.dryers.push(new Dryer(this.laundromat));
+		
 		this.customers = [];
 
 		console.log("Congrats, you now own a laundromat! Your rent is $500/month, and you own 3 machines: a change machine, a washer, and a dryer. You have $6,000 saved up just for this laundromat. Good luck!");
 		console.log(this.laundromat.machines.washers[0]);
 		console.log(this.laundromat.machines.dryers[0]);
 		console.log(this.laundromat.machines.changeMachines[0]);
+	}
 
-		this.speed = 100;
-
+	startTime(){
 		var _this = this;
 		this.interval = window.setInterval(function(){
 			_this.gameLoop();
 		}, this.speed);
+	}
+
+	pauseTime(){
+		window.clearInterval(this.interval);
+	}
+
+	adjustTimeSpeed(speed){
+		this.speed = speed;
+		this.pauseTime();
+		this.startTime();
 	}
 
 	passTime(tickSize){
@@ -269,10 +358,15 @@ class Game {
 			console.log("Rent paid on " + this.month + "/" + this.day);
 		}
 
-		// Pass time on machines
+		// Pass time on machines, update ui if machine is broken
 		var machines = this.laundromat.machines.washers.concat(this.laundromat.machines.dryers);
 		for (var i = 0; i < machines.length; i++){
 			machines[i].passTime(tickSize);
+			this.ui.updateMachine(machines[i]);
+		}
+		machines = this.laundromat.machines.changeMachines;
+		for (var i = 0; i < machines.length; i++){
+			this.ui.updateMachine(machines[i]);
 		}
 
 		// Add customers
@@ -289,17 +383,6 @@ class Game {
 		console.log(this.month + "/" + this.day + ": You have $" + this.laundromat.money);
 	}
 
-}
-
-var game;
-
-function initGame(){
-	game = new Game();
-}
-
-function handleChangeEvent(changeMachineId, change){
-	var laundromat = game.laundromat;
-	laundromat.findMachine(changeMachineId).addChange(change);
 }
 
 
--- script.js
+++ script.js
@@ -1,29 +1,138 @@
-var player;
-
-function init(){
-	var stage = new createjs.Stage("demoCanvas");
-
-	player = new createjs.Shape();
-	player.graphics.beginFill("Violet")
-		.drawCircle(0, 0, 50);
-	player.x = 200;
-	player.y = 100;
-
-	stage.addChild(player);
-
-	stage.update();
-	
-	createjs.Ticker.setFPS(60);
-	createjs.Ticker.addEventListener("tick", stage);
-	
+class GridObjectShape extends createjs.Shape {
+	constructor(objId){
+		super();
+		this.objectId = objId;
+		this.working = true;
+	}
 }
 
-function go(){
-	initGame();
-	
-	createjs.Tween.get(player, { loop: false })
-		.to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
-		.to({ x: 200, y: 250 }, 500, createjs.Ease.getPowInOut(4))
-		.to({ x: 100, y: 200 }, 500, createjs.Ease.getPowInOut(2))
-		.to({ x: 200, y: 100 }, 1000, createjs.Ease.getPowInOut(2));
+class UI{
+	constructor(game){
+		this.grid = Laundromat.newEmptyLayout(20, 20);
+		this.game = game;
+		this.gridShapes = [];
+	}
+
+	gridWidth(){
+		return this.grid.length;
+	}
+	gridHeight(){
+		return this.grid[0].length;
+	}
+
+	initUI(canvasId){
+		this.gameScreen = new createjs.Stage(canvasId);
+
+		this.gridSquareSize = (this.gameScreen.canvas.height * .9) / this.gridHeight();
+		var gridTopMargin = this.gameScreen.canvas.height * .05;
+		var gridLeftMargin = this.gameScreen.canvas.height * .05;
+		var gridWidth = this.gridWidth() * this.gridSquareSize;
+		var gridHeight = this.gridHeight() * this.gridSquareSize
+
+		this.layoutSquare = new createjs.Shape();
+		this.layoutSquare.setBounds(gridLeftMargin, gridTopMargin, gridWidth, gridHeight);
+		this.layoutSquare.graphics.beginFill("Blue")
+			.drawRect(gridLeftMargin, gridTopMargin, gridWidth, gridHeight);
+
+		this.gameScreen.addChild(this.layoutSquare);
+
+		this.gameScreen.update();
+		
+		// createjs.Ticker.setFPS(60);
+		// createjs.Ticker.addEventListener("tick", stage);
+		
+	}
+
+	createObjectShape(objectId, top, left, working = true){
+		var obj = this.game.laundromat.findMachine(objectId);
+		var objShape = new GridObjectShape(objectId);
+		objShape.working = working;
+
+		var color;
+		if (!working){
+			color = "Red";
+		}else if (objectId.startsWith("C")){
+			color = "Green";
+		}else if (objectId.startsWith("W")){
+			color = "Turquoise";
+		}else if (objectId.startsWith("D")){
+			color = "Purple";
+		}
+		objShape.graphics.beginFill(color);
+
+		var gridBounds = this.layoutSquare.getBounds();
+		console.log(gridBounds);
+		objShape.x = left;
+		objShape.y = top;
+
+		console.log("Creating a shape with coordinates (" + objShape.x + ", " + objShape.y + ")");
+
+		objShape.graphics.drawRect(
+			gridBounds.x + (this.gridSquareSize * objShape.x), 
+			gridBounds.y + (this.gridSquareSize * objShape.y), 
+			this.gridSquareSize * obj.width, 
+			this.gridSquareSize * obj.height);
+
+		this.gridShapes.push(objShape);
+		return objShape;
+	}
+
+	findObjectShape(objectId){
+		return this.gridShapes.find(function(shape){
+			return (shape.objectId == objectId);
+		});
+	}
+
+	updateGrid(){
+		console.log("layout has changed, need to update UI.");
+		this.grid = this.game.laundromat.layout;
+
+		for (var i = 0; i < this.gridWidth(); i++){
+			for (var j = 0; j < this.gridHeight(); j++){
+				// no object or blocked
+				if (this.grid[i][j] == -1 || this.grid[i][j] == 1){
+					continue;
+				}
+				console.log(this.gameScreen);
+				this.gameScreen.addChild(this.createObjectShape(this.grid[i][j], i, j));
+			}
+		}
+		this.gameScreen.update();
+	}
+
+	updateLayout(){
+		console.log("UI has changed the layout, game needs to be updated.");
+	}
+
+	updateMachine(machine){
+		var shape = this.findObjectShape(machine.objectId);
+
+		if (shape.working && !machine.working){
+			this.gameScreen.removeChild(shape);
+			this.gridShapes = this.gridShapes.filter(function (shape){
+				return shape.objectId != machine.objectId;
+			});
+			this.gameScreen.addChild(this.createObjectShape(machine.objectId, shape.x, shape.y, false));
+			
+			this.gameScreen.update();
+		}else if (!shape.working && machine.working){
+			this.gameScreen.removeChild(shape);
+			this.gridShapes = this.gridShapes.filter(function(shape){
+				return shape.objectId != machine.objectId;
+			});
+			this.gameScreen.addChild(this.createObjectShape(machine.objectId, shape.x, shape.y, true));
+			
+			this.gameScreen.update();
+		}
+		console.log(this.gridShapes);
+	}
+
+	handleChangeEvent(changeMachineId, change){
+		this.game.laundromat.findMachine(changeMachineId).addChange(change);
+	}
+}
+var game;
+
+function initGameAndUI(){
+	game = new Game();
 }
